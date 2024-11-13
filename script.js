8cb8cf2c3933aad4e25ec61c63ec78913bccd5bf
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-11-13 12:04:07 +0530

    Updated

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.onscroll = () => {
 }
 
 const typed = new Typed('.multiple-text',  {
-    strings: ['Web Developer','Cloud Engineer', 'Front-end Developer'],
+    strings: ['Web Developer','Java Developer', 'Front-end Developer'],
     typeSpeed: 80,
     backSpeed: 80,
     backDelay: 1200,
